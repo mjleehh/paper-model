c7078de9f2e436cc0b771d7afbb48de6a6f4dc9c
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-03-01 22:04:19 +0100

    Index file updated.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 exports.readObj = require('./lib/obj/read-obj');
 exports.writeObj = require('./lib/obj/write-obj');
-exports.converters = require('./lib/converters');
+exports.converters = require('./lib/converters/converters');
+exports.readGraph = require('./lib/graph/read-graph');
+exports.readTree = require('./lib/tree/read-tree');
