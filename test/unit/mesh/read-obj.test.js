04aaac9548ab4eb0b17bae51b4d2cc510694fd85
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-03-02 19:02:35 +0100

    Renamed obj package to mesh.

--- test/unit/mesh/read-obj.test.js
+++ test/unit/mesh/read-obj.test.js
@@ -2,20 +2,20 @@ var self = require('../../../index');
 var fs = require('fs');
 var expect = require('chai').expect;
 
-describe('read-obj', function(){
+describe('readObj', function(){
     it('reads an empty file', function(){
         var expectedResult = {
             vertices: [],
             faces: []
         };
-        var mesh = self.readObj('');
+        var mesh = self.mesh.readObj('');
         expect(mesh).to.be.eql(expectedResult);
     });
 
     it('reads a file', function(){
         var expectedResult = JSON.parse(fs.readFileSync(__dirname + '/resources/box.json', {encoding: 'ascii'}));
         var buffer = fs.readFileSync(__dirname + '/resources/box.obj', {encoding: 'ascii'});
-        var mesh = self.readObj(buffer);
+        var mesh = self.mesh.readObj(buffer);
         expect(mesh).to.be.eql(expectedResult);
     });
 });
--- test/unit/mesh/write-obj.test.js
+++ test/unit/mesh/write-obj.test.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var expect = require('chai').expect;
 
 
-describe('write-obj', function(){
+describe('writeObj', function(){
     it('writes an empty mesh', function(){
-        var objData = self.writeObj({
+        var objData = self.mesh.writeObj({
             vertices: [],
             faces: []
         });
@@ -16,8 +16,8 @@ describe('write-obj', function(){
     it('writes a mesh', function(){
         var expectedResult = fs.readFileSync(__dirname + '/resources/box-plain.obj', {encoding: 'ascii'});
         var inputData = fs.readFileSync(__dirname + '/resources/box.obj', {encoding: 'ascii'});
-        var mesh = self.readObj(inputData);
-        var objData = self.writeObj(mesh);
+        var mesh = self.mesh.readObj(inputData);
+        var objData = self.mesh.writeObj(mesh);
         expect(objData).to.be.eql(expectedResult);
     });
 });
