6e29009aa9caa1d9e6f803e0ee0699a712766e78
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-03-02 19:03:48 +0100

    Fixed paths in index.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-exports.readObj = require('./lib/obj/read-obj');
-exports.writeObj = require('./lib/obj/write-obj');
+exports.mesh = require('./lib/mesh');
 exports.converters = require('./lib/converters/converters');
 exports.readGraph = require('./lib/graph/read-graph');
 exports.readTree = require('./lib/tree/read-tree');
+exports.surfaceFromMesh = require('./lib/surface/surface-from-mesh');
