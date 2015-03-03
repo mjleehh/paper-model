e36148a28b0dead168219d4135d600e52eb7f78f
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-03-03 12:37:46 +0100

    Added test for numFaces.

--- test/unit/surface/surface.test.js
+++ test/unit/surface/surface.test.js
@@ -1,9 +1,19 @@
 var self = require('../../../index');
 var fs = require('fs');
-//var expect = require('chai').expect;
+var expect = require('chai').expect;
 
 describe('Surface', function(){
-    it('has a face getter', function(){
+    it('has a number of faces', function(){
+        //var expectedResult =
+        // JSON.parse(fs.readFileSync(__dirname + '/resources/l-patch-2.json', {encoding: 'ascii'}));
+        var buffer = fs.readFileSync(__dirname + '/resources/l-patch-2.surface', {encoding: 'ascii'});
+        var surface = new self.surface.Surface(self.surface.readSurface(buffer));
+
+        var numFaces = surface.numFaces;
+        expect(numFaces).to.be.eql(3);
+    });
+
+    it('has a face get method', function(){
         //var expectedResult =
         // JSON.parse(fs.readFileSync(__dirname + '/resources/l-patch-2.json', {encoding: 'ascii'}));
         var buffer = fs.readFileSync(__dirname + '/resources/l-patch-2.surface', {encoding: 'ascii'});
