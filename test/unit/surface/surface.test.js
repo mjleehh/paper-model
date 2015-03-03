cd601c8e9dd2f6da10c56aa78d715af9687603ed
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-03-03 21:19:55 +0100

    Added test for edges property on surfaces.

--- test/unit/surface/surface.test.js
+++ test/unit/surface/surface.test.js
@@ -4,16 +4,13 @@ var expect = require('chai').expect;
 
 describe('Surface', function(){
     it('has a number of faces', function(){
-        //var expectedResult =
-        // JSON.parse(fs.readFileSync(__dirname + '/resources/l-patch-2.json', {encoding: 'ascii'}));
         var buffer = fs.readFileSync(__dirname + '/resources/l-patch-2.surface', {encoding: 'ascii'});
         var surface = new self.surface.Surface(self.surface.readSurface(buffer));
-
         var numFaces = surface.numFaces;
         expect(numFaces).to.be.eql(3);
     });
 
-    it('has a face get method', function(){
+    it('has single faces', function(){
         //var expectedResult =
         // JSON.parse(fs.readFileSync(__dirname + '/resources/l-patch-2.json', {encoding: 'ascii'}));
         var buffer = fs.readFileSync(__dirname + '/resources/l-patch-2.surface', {encoding: 'ascii'});
@@ -22,5 +19,25 @@ describe('Surface', function(){
         for (var i = 0; i < surface.numFaces; ++i) {
             surface.getFace(i);
         }
+    });
+
+    it('has edges', function(){
+        var expectedResult = [
+            [null, 0],
+            [null, 1],
+            [null, 2],
+            [3, 4],
+            [3, 4],
+            [null, 5],
+            [6, 8],
+            [null, 7],
+            [6, 8],
+            [null, 9],
+            [null, 10],
+            [null, 11]
+        ];
+        var buffer = fs.readFileSync(__dirname + '/resources/l-patch-2.surface', {encoding: 'ascii'});
+        var surface = new self.surface.Surface(self.surface.readSurface(buffer));
+        expect(surface.edges).to.be.eql(expectedResult);
     });
 });
