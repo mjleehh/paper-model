1800b8004eca5706b145e78fa91486fd640e1d9b
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-03-15 01:02:43 +0100

    Main function fixed.

--- test/unit/main/main.test.js
+++ test/unit/main/main.test.js
@@ -1,10 +1,15 @@
 var self = require('../../../index');
 var fs = require('fs');
-//var expect = require('chai').expect;
+var expect = require('chai').expect;
+
 
 describe('main', function(){
-    it('creates a paper model', function(){
+    it('creates a paper model of a box', function(){
+        var expectedResult = JSON.parse(
+            fs.readFileSync(__dirname + '/resources/box-paper-model.json', {encoding: 'ascii'}));
         var mesh = self.mesh.readObj(fs.readFileSync(__dirname + '/resources/box.obj', {encoding: 'ascii'}));
-        fs.writeFileSync(__dirname + '/output.obj', self.mesh.writeObj(self.createPaperModel(mesh)));
+        var paperModel = self.createPaperModel(mesh);
+
+        expect(paperModel).to.be.eql(expectedResult);
     });
 });
