77e75bd6beeab33020265a0b700758e13f2db2bc
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-03-24 01:07:22 +0100

    Added missing test case.

--- test/unit/algorithms/face-intersection.test.js
+++ test/unit/algorithms/face-intersection.test.js
@@ -7,8 +7,7 @@ var expect = require('chai').expect;
 
 describe('face intersection', function(){
     it('detects intersecting triangles', function(){
-        var buffer = fs.readFileSync(__dirname + '/resources/intersecting-triangles.obj', {encoding: 'ascii'});
-        var mesh = self.mesh.readObj(buffer);
+        var mesh = self.mesh.readObj(fs.readFileSync(__dirname + '/resources/intersecting-triangles.obj', {encoding: 'ascii'}));
         var triangle1 = _.map(mesh.faces[0].vertices, function(vertexIndex){
             return mesh.vertices[vertexIndex];
         });
@@ -18,9 +17,21 @@ describe('face intersection', function(){
         expect(self.algorithms.faces.doFacesIntersect(triangle1, triangle2)).to.be.true;
     });
 
+    it('considers connected triangles non intersecting', function(){
+        var mesh = self.mesh.readObj(
+            fs.readFileSync(__dirname + '/resources/connected-triangles.obj', {encoding: 'ascii'}));
+        var triangle1 = _.map(mesh.faces[0].vertices, function(vertexIndex){
+            return mesh.vertices[vertexIndex];
+        });
+        var triangle2 = _.map(mesh.faces[1].vertices, function(vertexIndex){
+            return mesh.vertices[vertexIndex];
+        });
+        expect(self.algorithms.faces.doFacesIntersect(triangle1, triangle2)).to.be.false;
+    });
+
     it('detects non intersecting triangles', function(){
-        var buffer = fs.readFileSync(__dirname + '/resources/non-intersecting-triangles.obj', {encoding: 'ascii'});
-        var mesh = self.mesh.readObj(buffer);
+        var mesh = self.mesh.readObj(
+            fs.readFileSync(__dirname + '/resources/non-intersecting-triangles.obj', {encoding: 'ascii'}));
         var triangle1 = _.map(mesh.faces[0].vertices, function(vertexIndex){
             return mesh.vertices[vertexIndex];
         });
