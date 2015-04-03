0204f0442f5f75cd8604d2b4df3e10ec4295e6a7
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-04-03 11:21:37 +0200

    Dump method added to test utils.

--- test/unit/util.js
+++ test/unit/util.js
@@ -9,6 +9,9 @@ module.exports = function(dirname){
         },
         jsonResource: function(name){
             return JSON.parse(fs.readFileSync(RESOURCES_DIR + name, {encoding: 'ascii'}));
+        },
+        dump: function(filename, buffer){
+            fs.writeFileSync(dirname + '/' + filename, buffer);
         }
     };
 };
