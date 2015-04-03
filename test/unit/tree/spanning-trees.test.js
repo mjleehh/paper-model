fd8ff84c108593c29c3556b9a287c4090d5d7874
Michael Jonathan Lee <code.mjlee@gmail.com>
2015-04-03 21:05:49 +0200

    Removed useless test cases.

--- test/unit/tree/spanning-trees.test.js
+++ test/unit/tree/spanning-trees.test.js
@@ -1,48 +1,6 @@
 var self = require('../../../index');
 var expect = require('chai').expect;
 
-describe('findSpanningTree', function(){
-    it('fails if the graph is empty', function(){
-        expect(function(){
-            var graph = new self.graph.Graph({
-                nodes: []
-            });
-            self.tree.spanningTrees(graph);
-        }).to.throw(Error);
-    });
+describe('spanningTrees', function(){
 
-    /*
-    it('converts trivial graphs', function(){
-        var graph = new self.graph.Graph({
-            nodes: [{
-                value: 'one',
-                neighbours: []
-            }]
-        });
-        var tree = self.tree.spanningTrees(graph);
-        expect(tree).to.be.eql({
-            nodes: [{
-                value: 'one',
-                children: []
-            }],
-            rootNode: 0
-        });
-    });
-    */
-
-    it('fails if graph has disconnected components', function(){
-        expect(function(){
-            var graph = new self.graph.Graph(
-                self.graph.readGraph(fs.readFileSync(
-                    __dirname + 'disconnected.graph')));
-            self.tree.spanningTrees(graph);
-        }).to.throw(Error);
-    });
-
-    /*it('finds spanning trees of triple triangle graph', function(){
-        var graph = new self.graph.Graph(
-            self.graph.readGraph(util.readAsciiResource('triple-tri.graph')));
-        //self.tree.spanningTrees(graph);
-        //expect(spanningTree).to.be.eql(expectedResult);
-    });*/
 });
